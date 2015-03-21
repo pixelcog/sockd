d2e741e04fc8954f7152254441f80de73b51c5e8
Mike Greiling <mike@pixelcog.com>
2015-03-21 14:28:48 -0500

    add simple list daemon example

---
+++ examples/listd.rb
@@ -0,0 +1,50 @@
+#!/usr/bin/env ruby
+
+require 'sockd'
+
+Sockd.run 'listd' do |sockd|
+
+  sockd.options = {
+    pid_path: '/tmp/listd.pid',
+    socket:   '/tmp/listd.sock'
+  }
+
+  array = []
+  index = 0
+
+  sockd.handle do |message, socket|
+    command, *params = message.split(' ')
+
+    response =
+      case command
+      when 'add'
+        array.push(*params)
+        sockd.log 'added ' + params.join(' ')
+        ''
+      when 'remove'
+        array.delete(*params)
+        sockd.log 'removed ' + params.join(' ')
+        index = 0
+        ''
+      when 'get'
+        if array.empty?
+          ''
+        else
+          value = array[index]
+          index = (index + 1) % array.size
+          value
+        end
+      when 'list'
+        array.join(' ') || ''
+      when 'reset'
+        array = []
+        index = 0
+        sockd.log 'list reset'
+        ''
+      else
+        'bad command'
+      end
+
+    socket.print response + "\r\n"
+  end
+end
